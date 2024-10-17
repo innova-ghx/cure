39e02ccd11ab09bea732687f322d48f67cb2e4c4
Core7551 <jeffersonhasford@gmail.com>
2024-10-17 10:08:45 +0000

    initial commit

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <router-view class="app__view"></router-view>
+</template>
+
+<style>
+.app {
+  width: 100%;
+}
+
+.app__view {
+  min-height: 100dvh;
+  width: 100%;
+}
+</style>
