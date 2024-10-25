4afd63ddc02db6b9d0104863cd82a5097c3dfdf0
wabisque <welseywebb35@gmail.com>
2024-10-25 06:58:47 +0000

    [implemented] password type for text field components

--- src/components/text-field.vue
+++ src/components/text-field.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, onActivated, onDeactivated, onUnmounted, ref } from 'vue';
 import IconComponent from '@/components/icon.vue';
 import { createUid } from '@/utilities/uid';
 
@@ -14,8 +14,12 @@ const props = defineProps({
 });
 
 const uid = createUid();
+let frame = null;
 
 const passwordVisible = ref(false);
+const fieldActionsOffset = ref(0);
+const fieldActionsRef = ref(null);
+const fieldRef = ref(null);
 
 const inputmode = computed(() => {
   switch(props.type) {
@@ -24,32 +28,66 @@ const inputmode = computed(() => {
 });
 const inputtype = computed(() => {
   switch(props.type) {
+    case 'password': return passwordVisible.value ? 'text' : 'password';
     default: return props.type;
   }
 });
+
+function loop() {
+  if(fieldActionsRef.value) {
+    fieldActionsOffset.value = fieldActionsRef.value.getBoundingClientRect().width;
+  }
+
+  frame = requestAnimationFrame(loop);
+}
+
+onActivated(() => loop());
+onDeactivated(() => cancelAnimationFrame(frame));
+onUnmounted(() => cancelAnimationFrame(frame));
+
+loop();
 </script>
 
 <template>
-  <div class="text-field-component">
+  <div
+    class="text-field-component"
+    :style="{'--x-text-field-component--field-actions-offset': `${fieldActionsOffset}px`}"
+  >
     <input
       class="text-field-component__field"
       :id="`text-field-component__field--${uid}`"
       :inputmode="inputmode"
+      ref="fieldRef"
       :type="inputtype"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
     >
 
-    <div class="text-field-component__field-actions">
-      <icon-component
+    <div
+      class="text-field-component__field-actions"
+      ref="fieldActionsRef"
+    >
+      <button
         class="text-field-component__field-actions__password"
-        :type="passwordVisible ? 'eye-slash' : 'eye'"
-        @click="passwordVisible = !passwordVisible;"
-        v-if="inputtype == 'password'"
-      ></icon-component>
+        :class="{'text-field-component__field-actions__password--active': passwordVisible}"
+        type="button"
+        @click="
+          passwordVisible = !passwordVisible;
+          fieldRef?.focus();
+        "
+        v-if="type == 'password'"
+      >
+        <icon-component
+          class="text-field-component__field-actions__password__icon"
+          :type="passwordVisible ? 'eye-slash' : 'eye'"
+        ></icon-component>
+      </button>
     </div>
 
-    <label class="text-field-component__label">
+    <label
+      class="text-field-component__label"
+      :for="`text-field-component__field--${uid}`"
+    >
       <slot name="label"></slot>
     </label>
 
@@ -67,18 +105,18 @@ const inputtype = computed(() => {
 .text-field-component {
   display: inline-grid;
   grid-template-columns: [error-start field-start field-actions-start label-start] 1fr [label-end label-actions-start] auto [error-end field-end field-actions-end label-actions-end];
-  grid-template-rows: [label-start label-actions-start] auto [label-end label-actions-end field-start] 1fr [field-end error-start] auto [error-end];
+  grid-template-rows: [label-start label-actions-start] auto [label-end label-actions-end field-start field-actions-start] 1fr [field-end error-start field-actions-end] auto [error-end];
 }
 
 .text-field-component__field {
   background-color: transparent;
   border: var(--px) solid rgb(var(--color-border));
-  border-radius: 0.75rem;
+  border-radius: 0.5rem;
   box-shadow: 0px 0px 0px 0px rgba(var(--color-primary), 0);
   grid-area: field;
   outline: 0px solid rgba(var(--color-primary), 0);
   outline-offset: 0.35rem;
-  padding: 0.5rem;
+  padding: 0.5rem calc(0.5rem + var(--x-text-field-component--field-actions-offset)) 0.5rem 0.5rem;
   transition-property: border, box-shadow, outline, outline-offset;
 }
 
@@ -88,6 +126,47 @@ const inputtype = computed(() => {
   outline-color: rgb(var(--color-primary));
   outline-offset: calc(-1 * var(--px));
   outline-width: calc(2 * var(--px));
+}
+
+.text-field-component__field-actions {
+  align-items: center;
+  display: flex;
+  gap: 0.35rem;
+  grid-area: field-actions;
+  margin-right: 0.5rem;
+  padding-left: 0.35rem;
+  place-self: center end;
+  pointer-events: none;
+}
+
+.text-field-component__field-actions:empty {
+  display: none;
+}
+
+.text-field-component__field-actions>* {
+  pointer-events: initial;
+}
+
+.text-field-component__field-actions__password {
+  background-color: rgba(var(--color-border), 0);
+  border: none;
+  border-radius: 100vmax;
+  cursor: pointer;
+  padding: 0.25rem;
+  transition-property: background-color, color;
+}
+
+.text-field-component__field-actions__password--active {
+  background-color: rgba(var(--color-primary), 0);
+  color: rgb(var(--color-primary));
+}
+
+.text-field-component__field-actions__password:hover {
+  background-color: rgba(var(--color-border), 0.15);
+}
+
+.text-field-component__field-actions__password--active:hover {
+  background-color: rgba(var(--color-primary), 0.15);
 }
 
 .text-field-component__label {
